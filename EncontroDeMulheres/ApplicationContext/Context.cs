0808d8b8ba63212399adeb6584495042c4310465
LeonePS <leonepereira00@gmail.com>
2021-11-01 10:31:50 -0300

    Projeto Encontro de Mulheres

---
+++ EncontroDeMulheres/ApplicationContext/Context.cs
@@ -0,0 +1,19 @@
+﻿using EncontroDeMulheres.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncontroDeMulheres.ApplicationContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Cliente> cliente { get; set; }
+
+    }
+}
---
+++ EncontroDeMulheres/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncontroDeMulheres.Models
+{
+    public class Cliente
+    {   [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string SobreNome { get; set; }
+        [Required]
+        public string CPF { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Required]
+        public string Foto { get; set; }
+    }
+}
